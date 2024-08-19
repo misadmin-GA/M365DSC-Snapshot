f0b15e82570fa5cdc745015d8a1b3bf138966328
misadmin-GA <reports@mis-solutions.com>
2024-08-19 11:41:55 -0400

    Added GetDSCWorkloads function

--- HelperModule/HelperModule.psm1
+++ HelperModule/HelperModule.psm1
@@ -27,3 +27,121 @@ function Generate365DSCCertificate
 {
     Set-M365DSCAgentCertificateConfiguration -ForceRenew
 }
+
+function Get-M365DSCWorkloads
+{
+    [CmdletBinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param
+    (
+        [Parameter(Mandatory = $true, Position = 1)]
+        [System.Array]
+        $ResNames = @()
+    )
+
+    [Array]$workloads = @()
+    foreach ($res in $ResNames)
+    {
+        switch ($res.Substring(0, 2).ToUpper())
+        {
+            'AA'
+            {
+                if (-not $workloads.Name -or -not $workloads.Name.Contains('MicrosoftGraph'))
+                {
+                    $workloads += @{
+                        Name                 = 'MicrosoftGraph'
+                        AuthenticationMethod = $res.AuthenticationMethod
+                    }
+                }
+            }
+            'EX'
+            {
+                if (-not $workloads.Name -or -not $workloads.Name.Contains('ExchangeOnline'))
+                {
+                    $workloads += @{
+                        Name                 = 'ExchangeOnline'
+                        AuthenticationMethod = $res.AuthenticationMethod
+                    }
+                }
+            }
+            'In'
+            {
+                if (-not $workloads.Name -or -not $workloads.Name.Contains('MicrosoftGraph'))
+                {
+                    $workloads += @{
+                        Name                 = 'MicrosoftGraph'
+                        AuthenticationMethod = $res.AuthenticationMethod
+                    }
+                }
+            }
+            'O3'
+            {
+                if (-not $workloads.Name -or -not $workloads.Name.Contains('MicrosoftGraph') -and $res -eq 'O365Group')
+                {
+                    $workloads += @{
+                        Name                 = 'MicrosoftGraph'
+                        AuthenticationMethod = $res.AuthenticationMethod
+                    }
+                }
+                elseif (-not $workloads.Name -or -not $workloads.Name.Contains('ExchangeOnline'))
+                {
+                    $workloads += @{
+                        Name                 = 'ExchangeOnline'
+                        AuthenticationMethod = $res.AuthenticationMethod
+                    }
+                }
+            }
+            'OD'
+            {
+                if (-not $workloads.Name -or -not $workloads.Name.Contains('PnP'))
+                {
+                    $workloads += @{
+                        Name                 = 'PnP'
+                        AuthenticationMethod = $res.AuthenticationMethod
+                    }
+                }
+            }
+            'Pl'
+            {
+                if (-not $workloads.Name -or -not $workloads.Name.Contains('MicrosoftGraph'))
+                {
+                    $workloads += @{
+                        Name                 = 'MicrosoftGraph'
+                        AuthenticationMethod = $res.AuthenticationMethod
+                    }
+                }
+            }
+            'SP'
+            {
+                if (-not $workloads.Name -or -not $workloads.Name.Contains('PnP'))
+                {
+                    $workloads += @{
+                        Name                 = 'PnP'
+                        AuthenticationMethod = $res.AuthenticationMethod
+                    }
+                }
+            }
+            'SC'
+            {
+                if (-not $workloads.Name -or -not $workloads.Name.Contains('SecurityComplianceCenter'))
+                {
+                    $workloads += @{
+                        Name                 = 'SecurityComplianceCenter'
+                        AuthenticationMethod = $res.AuthenticationMethod
+                    }
+                }
+            }
+            'Te'
+            {
+                if (-not $workloads.Name -or -not $workloads.Name.Contains('MicrosoftTeams'))
+                {
+                    $workloads += @{
+                        Name                 = 'MicrosoftTeams'
+                        AuthenticationMethod = $res.AuthenticationMethod
+                    }
+                }
+            }
+        }
+    }
+    return ($workloads | Sort-Object {$_.Name})
+}
